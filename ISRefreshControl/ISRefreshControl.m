feda9e8d9446520aa1b615daeb30dc88a3ae0884
David Keegan <git@davidkeegan.com>
2013-03-20 15:02:24 -0700

    Use __typeof__ instead of typeof to avoid errors and warnings

--- ISRefreshControl/ISRefreshControl.m
+++ ISRefreshControl/ISRefreshControl.m
@@ -193,7 +193,7 @@ const CGFloat additionalTopInset = 50.f;
     }];
     
     if (self.topInsetsEnabled) {
-        __weak typeof(self) wself = self;
+        __weak __typeof__(self) wself = self;
         [self setTopInsetsEnabled:NO completion:^{
             UIScrollView *scrollView = (UIScrollView *)wself.superview;
             if (!scrollView.isDragging) {
@@ -223,7 +223,7 @@ const CGFloat additionalTopInset = 50.f;
     UIScrollView *scrollView = (id)self.superview;
     CGFloat diff = additionalTopInset * (enabled?1.f:-1.f);
     
-    __weak typeof(self) wself = self;
+    __weak __typeof__(self) wself = self;
     wself.animating = YES;
     
     [UIView animateWithDuration:.3f
